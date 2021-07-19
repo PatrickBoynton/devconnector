d9e86cae389f362f7a7aac8c85adb2ac0f5284cf
PatrickBoynton <jpboynton3@gmail.com>
2021-07-19 14:05:07 -0400

    Added the test routes as middleware.

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', (request, response) => {
     response.send('API RUNNING');
 });
 
+
+app.use('/api/users', require('./routes/api/users'));
+
+app.use('/api/auth', require('./routes/api/auth'));
+
+app.use('/api/profile', require('./routes/api/profile'));
+
+app.use('api/post', require('./routes/api/posts'));
+
+
 const PORT = process.env | 5000;
 
 app.listen(PORT, () => console.log(`Now listening on port: ${ PORT }`));
