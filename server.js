0169f385c7bc59551913e16c0705620bab9449fb
PatrickBoynton <jpboynton3@gmail.com>
2021-07-19 15:23:55 -0400

    Fixed a typo.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 app.use('/api/profile', require('./routes/api/profile'));
 
-app.use('api/post', require('./routes/api/posts'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 
 const PORT = process.env | 5000;
