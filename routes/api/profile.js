0c6c3ab0e24681098cdd37cd7378a33381b3c7f6
PatrickBoynton <jpboynton3@gmail.com>
2021-07-22 18:31:33 -0400

    Created a route to remove an experience.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -194,4 +194,26 @@ router.put('/experience', [auth, [
 });
 
 
+// @route DELETE api/profile/experience/exp_id
+// @description  Delete user experiences
+// @access       private
+router.delete('/experience/:exp_id', auth, async (request, response) => {
+    try {
+        const profile = await Profile.findOne({ user: request.user.id });
+
+        const removeIndex = profile.experience.map(item => item.id)
+            .indexOf(request.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        response.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        response.status(500).send('Internal server error.');
+    }
+});
+
+
 module.exports = router;
